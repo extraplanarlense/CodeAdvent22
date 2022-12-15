03ec585216ebbeff0021d38397c97e5fb108d0d9
extraplanarlense <helf.eike@gmail.com>
2022-12-15 15:12:05 +0100

    Problem 1 solved (rename) Problem 2 solved

---
+++ Day01/Day01b.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    with open('input.data') as f:
+        numbers_string  = f.readlines()
+    cal_list = list()
+    single_elf_cal = list()
+    buffer = 0;
+    for char in numbers_string:
+        if char != '\n':
+            number = int(char)
+            buffer += number
+        else:
+            single_elf_cal.append(buffer)
+            buffer = 0
+        single_elf_cal.sort()
+    print(single_elf_cal)
+    print("Max 3 Calories:", single_elf_cal[-1]+single_elf_cal[-2]+single_elf_cal[-3])
+
+
