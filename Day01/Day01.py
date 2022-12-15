767606a0888568d9b02bd918d5591e94d802ceec
extraplanarlense <helf.eike@gmail.com>
2022-12-15 14:45:53 +0100

    Problem 1 solved

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -1,29 +1,27 @@
 if __name__ == '__main__':
     with open('input.data') as f:
         numbers_string  = f.readlines()
-    integer_map = map(int, numbers_string)
-    numbers = list(integer_map)
-    counter = 0
-    buffer = -1
-    for current_number in numbers:
-        if (buffer != -1):
-            if current_number > buffer:
-                counter += 1
-            buffer = current_number
+    cal_list = list()
+    single_elf = list()
+    buffer = 0;
+    for char in numbers_string:
+        if char != '\n':
+            number = int(char)
+            single_elf.append(number)
+            buffer += number
         else:
-            buffer = current_number
-    print("Part 1: ", counter)
-    counter = 0
-    buffer = -1
-    for idx in range(len(numbers)):
-        if (buffer != -1):
-            if idx < (len(numbers) - 2):
-                if numbers[idx]+ numbers[idx + 1] + numbers[idx + 2] > buffer:
-                    counter += 1
-                buffer = numbers[idx]+ numbers[idx + 1] + numbers[idx + 2]
-        else:
-            buffer = numbers[idx]+ numbers[idx + 1] + numbers[idx + 2]
-    print("Part 2: ", counter)
+            single_elf.append(buffer)
+            cal_list.append(single_elf)
+            single_elf = list()
+            buffer = 0
+    max = 0
+    max_pos = 0
+    for pos, Anumber in enumerate(cal_list):
+        if Anumber[-1] > max:
+            max = Anumber[-1]
+            max_pos = pos
+    print(cal_list)
+    print(max_pos, max)
 
 
 
